5cb4b32bc0b631d463f7eae6422ddf493db57048
joncaddey <joncaddey@gmail.com>
2011-10-23 18:22:48 +0000

    Grossly misinterpreted what a pageInfo object should be.  Will fix.

---
+++ Web Spider/src/PageInfo.java
@@ -0,0 +1,34 @@
+import java.util.List;
+import java.util.Set;
+
+/*
+ * OS Archons
+ * 
+ * Jonathan Caddey
+ * 
+ * Travis Jensen
+ * 
+ * 2011-10-22
+ */
+
+/**
+ * Holds information about this page.
+ * 
+ * @author joncaddey
+ * @version 1.0
+ */
+public class PageInfo {
+
+	
+	/**
+	 * The URL this describes.
+	 */
+	private final String my_URL;
+	
+	private int my_total_links;
+	
+	private int my_total_words;
+
+	public PageInfo(final String the_URL)
+	
+}
